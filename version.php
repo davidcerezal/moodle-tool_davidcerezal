91233ee0fb5fc15bd705993113ebacbe3db74a79
David Cerezal Landa <david.cerezal77@gmail.com>
2024-02-08 15:56:45 +0100

    MP-1333 initial version of plugin

---
+++ version.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of the tool_datewatch plugin for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * tool_davidcerezal
+ *
+ * @package    tool_davidcerezal
+ * @copyright  2024 David Cerezal
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'tool_davidcerezal';
+$plugin->release   = '4.2.3';
+$plugin->version   = 2024020800;
+$plugin->requires  = 2020061500.00;
+$plugin->maturity  = MATURITY_ALPHA;
+$plugin->supported = [39, 403];
+
